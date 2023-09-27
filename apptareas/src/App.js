f76cf7f8f6484852a5eaf4888e9a2dc0b9bd9c69
Roman Ricciardi <romanezequielr97@gmail.com>
2023-09-26 22:40:11 -0300

    eliminar tareas

--- apptareas/src/App.js
+++ apptareas/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
-import TareaFormulario from "./componentes/TareaFormulario";
+import ListaDeTareas from "./componentes/ListaDeTareas";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       </div>
       <div className="tareas-lista-principal">
         <h1>Mis Tareas</h1>
-        <TareaFormulario />
+        <ListaDeTareas />
       </div>
     </div>
   );
